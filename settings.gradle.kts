6d7f37d48065ca318963c6ef762fc27103ecd834
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-05 17:36:35 +0900

    feat : VocaScrap android project init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "VocaScrap_Server"
---
+++ src/main/kotlin/com/example/vocascrap_server/VocaScrapServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vocascrap_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VocaScrapServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<VocaScrapServerApplication>(*args)
+}
